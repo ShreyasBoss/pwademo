65bf37afbdc9d183bbd9d71439151969f7f8046e
Shreyas Deshpande <51990143+ShreyasBoss@users.noreply.github.com>
2022-05-12 16:18:56 +0530

    trying pwa

---
+++ app.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", () => {
+  getDog();
+  registerSW();
+});
+
+// Register the Service Worker
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("serviceworker.js");
+    } catch (e) {
+      console.log("SW registration failed");
+    }
+  }
+}
+
+async function getDog() {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random");
+
+  const data = await res.json();
+
+  document.querySelector("img").src = data.message;
+}
