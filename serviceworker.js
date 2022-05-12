d08a802ca5060e4490faf29535e8295544b49da2
Shreyas Deshpande <51990143+ShreyasBoss@users.noreply.github.com>
2022-05-12 18:38:48 +0530

    sd

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 var cacheName = "pwa";
-var filesToCache = ["/", "/index.html", "/images", "/app.js"];
+var filesToCache = ["https://shreyasboss.github.io/pwademo/", "https://shreyasboss.github.io/pwademo/index.html", "https://shreyasboss.github.io/pwademo/app.js"];
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
